7da365c9af1077cecd6e6d1438870699b62ac604
vlu00 <vlu00@stuy.edu>
2018-11-09 10:14:25 -0500

    Copied my version into OldWordSearch.java and started to setup WordSearch.java

---
+++ OldWordSearch.java
@@ -0,0 +1,93 @@
+public class OldWordSearch {
+  private char [][] data;
+
+  public OldWordSearch(int rows, int cols) {
+    data = new char[rows][cols];
+    clear();
+  }
+
+  public void clear() {
+    for (int i = 0; i < data.length; i++) {
+      for (int n = 0; n < data[i].length; n++) {
+        data[i][n] = '_';
+      }
+    }
+  }
+
+  public String toString() {
+    String s = "";
+    for (int i = 0; i < data.length; i++) {
+      for (int n = 0; n < data[i].length; n++) {
+        s = s + data[i][n] + " ";
+      }
+      s = s + "\n";
+    }
+    return s;
+  }
+
+  /**Checks if the WordGrid can hold words and if the given position is valid
+     *@return true if the given position parameters are valid. If the given
+     *position parameters are not valid, return false.
+     */
+  public boolean valid(String word, int row, int col) {
+    return data.length == 0 || data[0].length == 0 ||
+           row < 0 || col < 0 ||
+           row > data.length -1 || col > data[1].length -1;
+  }
+
+  public boolean addWordHorizontal(String word, int row, int col) {
+    boolean complete = true;
+    if (valid(word, row, col) || col + word.length() > data[0].length) {
+      return false;
+    }
+    else {
+      for (int i = 0; i < word.length(); i++) {
+        if (data[row][col+i] != '_' && data[row][col+i] != word.charAt(i)) {
+            return false;
+        }
+      }
+      for (int i = 0; i < word.length(); i++) {
+          data[row][col+i] = word.charAt(i);
+      }
+    }
+    return complete;
+  }
+
+  public boolean addWordVertical(String word, int row, int col) {
+    boolean complete = true;
+    if (valid(word, row, col) || row + word.length() > data.length ) {
+      return false;
+    }
+    else {
+      for (int i = 0; i < word.length(); i++) {
+        if (data[row+i][col] != '_' && data[row+i][col] != word.charAt(i)) {
+            return false;
+        }
+      }
+      for (int i = 0; i < word.length(); i++) {
+          data[row+i][col] = word.charAt(i);
+      }
+    }
+    return complete;
+  }
+
+  public boolean addWordDiagonal(String word,int row, int col){
+    boolean complete = true;
+    if (valid(word, row, col) || row + word.length() > data.length ||
+        col + word.length() > data[0].length) {
+      return false;
+    }
+    else {
+      for (int i = 0; i < word.length(); i++) {
+        if (data[row+i][col+i] != '_' && data[row+i][col+i] != word.charAt(i)) {
+            return false;
+        }
+      }
+      for (int i = 0; i < word.length(); i++) {
+          data[row+i][col+i] = word.charAt(i);
+      }
+    }
+    return complete;
+  }
+
+}
--- WordSearch.java
+++ WordSearch.java
@@ -1,93 +1,28 @@
-public class WordSearch {
-  private char [][] data;
+import java.util.*; //random, scanner, arraylist
+import java.io.*; //file, filenotfoundexception
+public class WordSearch{
+    private char[][]data;
 
-  public WordSearch(int rows, int cols) {
-    data = new char[rows][cols];
-    clear();
-  }
+    //the random seed used to produce this WordSearch
+    private int seed;
 
-  public void clear() {
-    for (int i = 0; i < data.length; i++) {
-      for (int n = 0; n < data[i].length; n++) {
-        data[i][n] = '_';
-      }
-    }
-  }
+    //a random Object to unify your random calls
+    private Random randgen;
 
-  public String toString() {
-    String s = "";
-    for (int i = 0; i < data.length; i++) {
-      for (int n = 0; n < data[i].length; n++) {
-        s = s + data[i][n] + " ";
-      }
-      s = s + "\n";
-    }
-    return s;
-  }
+    //all words from a text file get added to wordsToAdd, indicating that they have not yet been added
+    private ArrayList<String>wordsToAdd;
 
-  /**Checks if the WordGrid can hold words and if the given position is valid
-     *@return true if the given position parameters are valid. If the given
-     *position parameters are not valid, return false. 
-     */
-  public boolean valid(String word, int row, int col) {
-    return data.length == 0 || data[0].length == 0 ||
-           row < 0 || col < 0 ||
-           row > data.length -1 || col > data[1].length -1;
-  }
+    //all words that were successfully added get moved into wordsAdded.
+    private ArrayList<String>wordsAdded;
 
-  public boolean addWordHorizontal(String word, int row, int col) {
-    boolean complete = true;
-    if (valid(word, row, col) || col + word.length() > data[0].length) {
-      return false;
+    public WordSearch( int rows, int cols, String fileName) {
+      
     }
-    else {
-      for (int i = 0; i < word.length(); i++) {
-        if (data[row][col+i] != '_' && data[row][col+i] != word.charAt(i)) {
-            return false;
-        }
-      }
-      for (int i = 0; i < word.length(); i++) {
-          data[row][col+i] = word.charAt(i);
-      }
-    }
-    return complete;
-  }
+    public WordSearch( int rows, int cols, String fileName, int randSeed)
+    Both will read in the word text file, then run addAllWords(). Do not fill in random letters after.
 
-  public boolean addWordVertical(String word, int row, int col) {
-    boolean complete = true;
-    if (valid(word, row, col) || row + word.length() > data.length ) {
-      return false;
-    }
-    else {
-      for (int i = 0; i < word.length(); i++) {
-        if (data[row+i][col] != '_' && data[row+i][col] != word.charAt(i)) {
-            return false;
-        }
-      }
-      for (int i = 0; i < word.length(); i++) {
-          data[row+i][col] = word.charAt(i);
-      }
-    }
-    return complete;
-  }
-
-  public boolean addWordDiagonal(String word,int row, int col){
-    boolean complete = true;
-    if (valid(word, row, col) || row + word.length() > data.length ||
-        col + word.length() > data[0].length) {
-      return false;
-    }
-    else {
-      for (int i = 0; i < word.length(); i++) {
-        if (data[row+i][col+i] != '_' && data[row+i][col+i] != word.charAt(i)) {
-            return false;
-        }
-      }
-      for (int i = 0; i < word.length(); i++) {
-          data[row+i][col+i] = word.charAt(i);
-      }
-    }
-    return complete;
-  }
+//toString should print in the following format:
+//use '|' as left/right boundaries of the grid.
+//One long line of comma separated words after the string "Words: "
 
 }
